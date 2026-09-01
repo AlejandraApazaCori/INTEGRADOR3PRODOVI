79d3295b77aeaa0c8da995cd53ece3723bb595ee
AlejandraApazaCori <alejandraapazacori@gmail.com>
2026-09-01 19:42:41 -0400

    card22

--- resources/views/clientes/analiticas.blade.php
+++ resources/views/clientes/analiticas.blade.php
@@ -17,19 +17,7 @@
     </header>
     <main class="analytics-content">
     <section class="analytics-panel {{ $campaniaIniciada ? '' : 'is-waiting' }}">
-    <div class="flex flex-col sm:flex-row sm:items-center sm:justify-between mb-6">
 
-        @if($campaniaIniciada)
-            <div class="flex space-x-3 mt-4 sm:mt-0">
-                <button onclick="exportData(event)" class="flex items-center justify-center px-4 py-2 border border-gray-300 rounded-lg shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"/>
-                    </svg>
-                    Exportar
-                </button>
-            </div>
-        @endif
-    </div>
 
     @if($campaniaIniciada)
         <div id="metricsContainer">
